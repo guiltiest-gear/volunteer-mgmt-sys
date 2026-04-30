67d9f9d25cbacd68ec154889da47b0b1b5c846fc
Felix Giang <felix.k.giang@gmail.com>
2026-04-30 14:16:03 -0700

    Refactor navigation and alert functions in home-page.js

---
+++ home-page.js
@@ -0,0 +1,24 @@
+/*
+  We will need to change the alerts later on, but this is how it will work in the development stage.
+*/
+
+//page navigation
+function navigate(page)
+{
+  alert("Navigating to " + page);
+}
+//function to access profile
+function profile()
+{
+  alert("Opening profile...");
+}
+//sign out function
+function signOut()
+{
+  //prompt user to confirm logout
+  const confirmLogout = confirm("Are you sure you want to sign out?");
+  if (confirmLogout)
+  {
+    alert("Signed out!");
+  }
+}
