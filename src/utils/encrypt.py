7753a57bb8a5e493a75604fd8652a2f820cf0c20
Jade Barker <guiltier.gear@gmail.com>
2026-04-30 17:35:52 -0700

    feat(api): create password_hash function

---
+++ src/utils/encrypt.py
@@ -0,0 +1,13 @@
+import hashlib
+import secrets
+
+def password_hash(password: str) -> str:
+    """Takes in a password, adds salt, and returns the hash as a string"""
+    # Generate 32 byte long salt
+    salt: str = secrets.token_hex(32)
+
+    # Encode string to bytes for sha256 function
+    hash_input: bytes = (password + salt).encode()
+
+    # Return hex digest of hash
+    return hashlib.sha256(hash_input, usedforsecurity=True).hexdigest()
