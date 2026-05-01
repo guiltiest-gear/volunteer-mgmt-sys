191c3ebc2ce65a0d84cca00f2adb5553d3c8d348
Jade Barker <guiltier.gear@gmail.com>
2026-05-01 14:28:57 -0700

    refactor(encrypt): import only what's needed from secrets and hashlib

--- src/utils/encrypt.py
+++ src/utils/encrypt.py
@@ -1,13 +1,13 @@
-import hashlib
-import secrets
+from hashlib import sha256
+from secrets import token_hex
 
 def password_hash(password: str) -> tuple[str, str]:
     """Takes in a password, adds salt, and returns the hash as a string"""
     # Generate 32 byte long salt
-    salt: str = secrets.token_hex(32)
+    salt: str = token_hex(32)
 
     # Encode string to bytes for sha256 function
     hash_input: bytes = (password + salt).encode()
 
     # Return hex digest of hash
-    return salt, hashlib.sha256(hash_input, usedforsecurity=True).hexdigest()
+    return salt, sha256(hash_input, usedforsecurity=True).hexdigest()
