ef89abd2b6398567128bc73e88a61787999d90fc
Jade Barker <guiltier.gear@gmail.com>
2026-05-01 14:10:25 -0700

    fix(encrypt): return salt in tuple
    I don't know how I managed to forget to return the salt, given this is
    critical for password validation and such. Regardless, should now be
    fixed.

--- src/utils/encrypt.py
+++ src/utils/encrypt.py
@@ -1,7 +1,7 @@
 import hashlib
 import secrets
 
-def password_hash(password: str) -> str:
+def password_hash(password: str) -> tuple[str, str]:
     """Takes in a password, adds salt, and returns the hash as a string"""
     # Generate 32 byte long salt
     salt: str = secrets.token_hex(32)
@@ -10,4 +10,4 @@ def password_hash(password: str) -> str:
     hash_input: bytes = (password + salt).encode()
 
     # Return hex digest of hash
-    return hashlib.sha256(hash_input, usedforsecurity=True).hexdigest()
+    return salt, hashlib.sha256(hash_input, usedforsecurity=True).hexdigest()
