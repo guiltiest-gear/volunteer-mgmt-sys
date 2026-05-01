03361f15531da45b42975de759dbfaa0d7fa7338
Jade Barker <guiltier.gear@gmail.com>
2026-05-01 14:29:28 -0700

    docs(encrypt): improve comments and docstring for password_hash

--- src/utils/encrypt.py
+++ src/utils/encrypt.py
@@ -2,11 +2,16 @@ from hashlib import sha256
 from secrets import token_hex
 
 def password_hash(password: str) -> tuple[str, str]:
-    """Takes in a password, adds salt, and returns the hash as a string"""
+    """
+    Return a tuple of the salt and hashed password.
+
+    The first element is the salt, and the second is the hex digest,
+    both of them being strings.
+    """
     # Generate 32 byte long salt
     salt: str = token_hex(32)
 
-    # Encode string to bytes for sha256 function
+    # Concatenate and encode password and salt strings to bytes for sha256 function
     hash_input: bytes = (password + salt).encode()
 
     # Return hex digest of hash
