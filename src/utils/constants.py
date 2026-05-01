158be789a400257062b4ce16f6b000b1c3ede5b9
Jade Barker <guiltier.gear@gmail.com>
2026-05-01 14:56:15 -0700

    fix(constants): get constants from env vars

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,4 +1,6 @@
-MYSQL_URL = "localhost"
-MYSQL_USER = "mysql"
-MYSQL_PASSWORD = "password"  # TODO: Figure out how to get password from environment
-MYSQL_DATABASE = "volunteerDB"
+import os
+
+MYSQL_URL = os.getenv("MYSQL_URL", default="localhost")
+MYSQL_USER = os.getenv("MYSQL_USER", default="mysql")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", default="volunteerDB")
