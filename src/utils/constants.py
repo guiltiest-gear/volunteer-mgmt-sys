1ad1e3f9205d9988276f9473cfb96424c40d944a
Jade Barker <guiltier.gear@gmail.com>
2026-05-14 16:10:11 -0700

    fix!: massive rewrite using preact + vite

--- src/utils/constants.py
+++
@@ -1,6 +0,0 @@
-import os
-
-MYSQL_URL = os.getenv("MYSQL_URL", default="localhost")
-MYSQL_USER = os.getenv("MYSQL_USER", default="mysql")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", default="")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", default="volunteerDB")
