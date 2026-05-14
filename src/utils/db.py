acea8eb7079c269ac9b8e08a254e42e5bb68c511
Jade Barker <guiltier.gear@gmail.com>
2026-05-13 20:39:20 -0700

    feat: create init_tables function

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,4 +1,39 @@
+import pymysql.cursors
+
+from .constants import *
+
 def init_tables() -> None:
     """Create the tables in the database"""
-    pass
+    connection = pymysql.connect(
+        host=MYSQL_URL,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DATABASE,
+    )
 
+    with connection:
+        # Create the user_auth table
+        # This needs to be done first as user_info depends on user_auth
+        with connection.cursor() as cursor:
+            # sql = "CREATE TABLE IF NOT EXISTS `user_auth` (`pw_id` INT AUTO_INCREMENT PRIMARY_KEY, `pw_salt` CHAR(64) NOT NULL, `pw_hash` CHAR(64) NOT NULL)"
+            sql: str = (
+                "CREATE TABLE IF NOT EXISTS `user_auth` ("
+                "`pw_id` INT AUTO_INCREMENT PRIMARY_KEY,"
+                "`pw_salt` CHAR(64) NOT NULL,"
+                "`pw_hash` CHAR(64) NOT NULL"
+                ")"
+            )
+            _ = cursor.execute(sql)
+        connection.commit()
+
+        # Create the user_info table
+        with connection.cursor() as cursor:
+            sql: str = (
+                "CREATE TABLE IF NOT EXISTS `user_info` ("
+                "`user_id` INT AUTO_INCREMENT PRIMARY_KEY,"
+                "`name` VARCHAR(255)"
+                "CONSTRAINT `fk_auth` FOREIGN KEY (`pw_id`) REFERENCES user_auth(`pw_id`)"
+                ")"
+            )
+            _ = cursor.execute(sql)
+        connection.commit()
