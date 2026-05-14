ad27b7b0225a61ce3c1755da11331eaf88b526f8
Jade Barker <guiltier.gear@gmail.com>
2026-05-13 20:56:37 -0700

    refactor: fix various warnings from pyright and ruff

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,4 +1,4 @@
-import pymysql.cursors
+import pymysql.cursors  # pyright: ignore[reportUnusedImport]
 
 from .constants import *
 
@@ -16,24 +16,24 @@ def init_tables() -> None:
         # This needs to be done first as user_info depends on user_auth
         with connection.cursor() as cursor:
             # sql = "CREATE TABLE IF NOT EXISTS `user_auth` (`pw_id` INT AUTO_INCREMENT PRIMARY_KEY, `pw_salt` CHAR(64) NOT NULL, `pw_hash` CHAR(64) NOT NULL)"
-            sql: str = (
+            auth_sql: str = (
                 "CREATE TABLE IF NOT EXISTS `user_auth` ("
                 "`pw_id` INT AUTO_INCREMENT PRIMARY_KEY,"
                 "`pw_salt` CHAR(64) NOT NULL,"
                 "`pw_hash` CHAR(64) NOT NULL"
                 ")"
             )
-            _ = cursor.execute(sql)
+            _ = cursor.execute(auth_sql)
         connection.commit()
 
         # Create the user_info table
         with connection.cursor() as cursor:
-            sql: str = (
+            user_sql: str = (
                 "CREATE TABLE IF NOT EXISTS `user_info` ("
                 "`user_id` INT AUTO_INCREMENT PRIMARY_KEY,"
                 "`name` VARCHAR(255)"
                 "CONSTRAINT `fk_auth` FOREIGN KEY (`pw_id`) REFERENCES user_auth(`pw_id`)"
                 ")"
             )
-            _ = cursor.execute(sql)
+            _ = cursor.execute(user_sql)
         connection.commit()
