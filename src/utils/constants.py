0ae9a334b4fd1c8196684d5f63c9131f22ef30a3
Jade Barker <guiltier.gear@gmail.com>
2026-05-13 20:38:07 -0700

    fix: set MYSQL_PASSWORD to an empty string if not defined

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,5 +2,5 @@ import os
 
 MYSQL_URL = os.getenv("MYSQL_URL", default="localhost")
 MYSQL_USER = os.getenv("MYSQL_USER", default="mysql")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", default="")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", default="volunteerDB")
