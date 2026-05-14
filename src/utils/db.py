50c0db89a56873f87b4cc10b914a21db689eb6d6
Jade Barker <guiltier.gear@gmail.com>
2026-05-13 20:38:57 -0700

    fix: drop check_tables function

--- src/utils/db.py
+++ src/utils/db.py
@@ -2,6 +2,3 @@ def init_tables() -> None:
     """Create the tables in the database"""
     pass
 
-def check_tables() -> None:
-    """Check if the tables we need are in the database and create if otherwise"""
-    pass
